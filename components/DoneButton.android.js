fa7d8f9e988e50b69d0aeb911dcf1e8b78be577d
amoreno <amoreno@apsl.net>
2017-10-19 10:21:02 +0200

    Solve visualization bug with done button on iOS

--- components/DoneButton.android.js
+++ components/DoneButton.android.js
@@ -1,23 +1,24 @@
 import React from 'react'
-import {
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native'
 
 export const DoneButton = ({
-  styles, onDoneBtnClick, onNextBtnClick,
-  rightTextColor, isDoneBtnShow,
-  doneBtnLabel, nextBtnLabel,
+  styles,
+  onDoneBtnClick,
+  onNextBtnClick,
+  rightTextColor,
+  isDoneBtnShow,
+  doneBtnLabel,
+  nextBtnLabel
 }) => {
   return (
     <View style={[styles.btnContainer, { height: 0, paddingBottom: 5 }]}>
-      <TouchableOpacity style={styles.full}
-        onPress={ isDoneBtnShow ? onDoneBtnClick : onNextBtnClick}
+      <TouchableOpacity
+        style={styles.full}
+        onPress={isDoneBtnShow ? onDoneBtnClick : onNextBtnClick}
       >
-       <Text style={[styles.nextButtonText, { color: rightTextColor }]}>
-         {isDoneBtnShow ? doneBtnLabel : nextBtnLabel}
-       </Text>
+        <Text style={[styles.nextButtonText, { color: rightTextColor }]}>
+          {isDoneBtnShow ? doneBtnLabel : nextBtnLabel}
+        </Text>
       </TouchableOpacity>
     </View>
   )
--- components/DoneButton.ios.js
+++ components/DoneButton.ios.js
@@ -1,43 +1,61 @@
 import React from 'react'
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  Animated
-} from 'react-native';
+import { Text, View, TouchableOpacity, Animated } from 'react-native'
 
 export const DoneButton = ({
-  styles, onDoneBtnClick, onNextBtnClick,
-  rightTextColor, isDoneBtnShow,
-  doneBtnLabel, nextBtnLabel,
-  doneFadeOpacity, skipFadeOpacity, nextOpacity
+  styles,
+  onDoneBtnClick,
+  onNextBtnClick,
+  rightTextColor,
+  isDoneBtnShow,
+  doneBtnLabel,
+  nextBtnLabel,
+  doneFadeOpacity,
+  skipFadeOpacity,
+  nextOpacity
 }) => {
   return (
     <View style={styles.btnContainer}>
-      <Animated.View style={[styles.full, { height: 0 }, {
-        opacity: doneFadeOpacity,
-        transform: [{
-          translateX: skipFadeOpacity.interpolate({
-            inputRange: [0, 1],
-            outputRange: [0, 20],
-          }),
-        }],
-      }]}
+      <Animated.View
+        style={[
+          styles.full,
+          { height: 0 },
+          {
+            opacity: doneFadeOpacity,
+            transform: [
+              {
+                translateX: skipFadeOpacity.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 20]
+                })
+              }
+            ]
+          }
+        ]}
       >
         <View style={styles.full}>
-          <Text style={[styles.controllText, {
-            color: rightTextColor, paddingRight: 30,
-          }]}>
+          <Text
+            style={[
+              styles.controllText,
+              {
+                color: rightTextColor,
+                paddingLeft: 30
+              }
+            ]}
+          >
             {doneBtnLabel}
           </Text>
         </View>
       </Animated.View>
-      <Animated.View style={[styles.full, { height: 0 }, { opacity: nextOpacity }]}>
-        <TouchableOpacity style={styles.full}
-          onPress={ isDoneBtnShow ? onDoneBtnClick : onNextBtnClick}>
-         <Text style={[styles.nextButtonText, { color: rightTextColor }]}>
-          {nextBtnLabel}
-        </Text>
+      <Animated.View
+        style={[styles.full, { height: 0 }, { opacity: nextOpacity }]}
+      >
+        <TouchableOpacity
+          style={styles.full}
+          onPress={isDoneBtnShow ? onDoneBtnClick : onNextBtnClick}
+        >
+          <Text style={[styles.nextButtonText, { color: rightTextColor }]}>
+            {nextBtnLabel}
+          </Text>
         </TouchableOpacity>
       </Animated.View>
     </View>
@@ -45,4 +63,3 @@ export const DoneButton = ({
 }
 
 export default DoneButton
-        
