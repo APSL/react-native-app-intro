62558c00171dfaf2baaf1b5c021a3252d16eab94
amoreno <amoreno@apsl.net>
2017-10-23 11:45:31 +0200

    Solve padding with Skip button on iOS

--- components/SkipButton.android.js
+++ components/SkipButton.android.js
@@ -1,25 +1,28 @@
 import React from 'react'
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  Animated
-} from 'react-native';
+import { Text, View, TouchableOpacity, Animated } from 'react-native'
 
 export const SkipButton = ({
-  styles, onSkipBtnClick, isSkipBtnShow,
+  styles,
+  onSkipBtnClick,
+  isSkipBtnShow,
   leftTextColor,
   skipBtnLabel,
   skipFadeOpacity
 }) => {
   return (
-    <View style={[styles.btnContainer, {
-        paddingBottom: 5,
-        opacity: isSkipBtnShow ? 1 : 0,
-      }]}>
+    <View
+      style={[
+        styles.btnContainer,
+        {
+          paddingBottom: 5,
+          opacity: isSkipBtnShow ? 1 : 0
+        }
+      ]}
+    >
       <TouchableOpacity
         style={styles.full}
-        onPress={isSkipBtnShow ? () => onSkipBtnClick() : null}>
+        onPress={isSkipBtnShow ? () => onSkipBtnClick() : null}
+      >
         <Text style={[styles.controllText, { color: leftTextColor }]}>
           {skipBtnLabel}
         </Text>
